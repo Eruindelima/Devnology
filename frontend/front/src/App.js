40046a927530702b7ca089d7a9a24bba381e5da8
Eruindelima <eruin.silva@gmail.com>
2023-03-18 12:42:02 -0300

    üpdates

---
+++ frontend/front/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Users from './componentes/Users';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+      <Users />
+    </div>
+  );
+}
+
+export default App;
